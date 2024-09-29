7b764c10ce83de9c864e0b7f1c4cdf456a42fab4
shu0o0 <96320419+shu0o0@users.noreply.github.com>
2024-09-29 10:21:32 +0800

    Update train.py

--- train.py
+++ train.py
@@ -34,10 +34,6 @@ Hforward = Hforward.cuda()
 lr = 0.001
 if method == 'ccnncgh':
     net = model.ccnncgh()
-if method == 'holonet':
-    net = model.holonet()
-if method == 'holoencoder':
-    net = model.holoencoder()
 
 criterion = nn.MSELoss()
 net = net.cuda()
@@ -48,21 +44,29 @@ init_phase = init_phase.cuda()
 optvars = [{'params': net.parameters()}]
 optimizier = torch.optim.Adam(optvars, lr=lr)
 
-trainpath = 'D:\\DIV2K_train_HR'
-validpath = 'D:\\DIV2K_valid_HR'
+trainpath = './data/train'
+validpath = './data/validation'
 l = []
 tl = []
 
+def psnr(img1, img2):
+    mse = torch.mean((img1 - img2) ** 2)
+    if mse < 1.0e-10:
+        return 100
+    PIXEL_MAX = 1.0
+    return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
+
 for k in trange(num):
+    currentpsnr = 0
     currenttloss = 0
     currentloss = 0
     for kk in range(trainnum):
         optimizier.zero_grad()
-
-        image_index = 100 + kk
+        
+        image_index = kk+100
         flip = numpy.random.randint(low=0, high=100)
 
-        input_image = tools.loadimage(path=trainpath, image_index=image_index, channel=2, flip=flip, m=m, n=n,
+        input_image = tools.loadimage(path=trainpath, image_index=image_index, channel=channel, flip=flip, m=m, n=n,
                                       convert=True, cuda=True)
 
         target_amp = torch.sqrt(input_image)
@@ -70,7 +74,6 @@ for k in trange(num):
         target_amp = target_amp.float()
 
         holo_phase, predict_phase = net(target_amp, init_phase, z, pad, pitch, wavelength, Hforward)
-
         slm_complex = torch.complex(torch.cos(holo_phase), torch.sin(holo_phase))
         recon_complex = propagation_ASM(u_in=slm_complex, z=-z, linear_conv=pad, feature_size=[pitch, pitch],
                                         wavelength=wavelength,
@@ -79,6 +82,7 @@ for k in trange(num):
         recon_amp = torch.abs(recon_complex)
         loss = criterion(recon_amp, target_amp)
 
+        print('iteration:', k, '{}/700'.format(kk),'loss', loss.item())
         currenttloss = currenttloss + loss.cpu().data.numpy()
         loss.backward()
         optimizier.step()
@@ -88,7 +92,7 @@ for k in trange(num):
     if k % 10 == 0:
         c = k
         b = '1\\' + method + str(c)
-        imgpath = b + '.png'
+        imgpath = './results/'+b + '.png'
         recon_amp = torch.squeeze(recon_amp)
         recon_amp = recon_amp.cpu().data.numpy()
         recon = recon_amp * recon_amp
@@ -100,7 +104,7 @@ for k in trange(num):
         cv2.imwrite(imgpath, pic)
 
         b = '3\\' + method + str(c)
-        imgpath = b + '.png'
+        imgpath = './results/'+b + '.png'
         phasepic = torch.squeeze(predict_phase)
         max_phs = 2 * torch.pi
         phasepic = ((phasepic + max_phs / 2) % max_phs) / max_phs
@@ -112,7 +116,7 @@ for k in trange(num):
             image_index = 801 + kk
             flip = numpy.random.randint(low=0, high=100)
 
-            input_image = tools.loadimage(path=validpath, image_index=image_index, channel=2, flip=flip, m=m, n=n,
+            input_image = tools.loadimage(path=validpath, image_index=image_index, channel=channel, flip=flip, m=m, n=n,
                                           convert=True, cuda=True)
 
             target_amp = torch.sqrt(input_image)
@@ -127,13 +131,15 @@ for k in trange(num):
                                             precomped_H=Hbackward)
 
             recon_amp = torch.abs(recon_complex)
+            psnrr = psnr(recon_amp, target_amp)
+            currentpsnr = currentpsnr + psnrr
             loss = criterion(recon_amp, target_amp)
             currentloss = currentloss + loss.cpu().data.numpy()
 
             if k % 10 == 0 and kk == 38:
                 c = k
                 b = '2\\' + method + str(c)
-                imgpath = b + '.png'
+                imgpath = './results/'+ b + '.png'
                 recon_amp = torch.squeeze(recon_amp)
                 recon_amp = recon_amp.cpu().data.numpy()
                 recon = recon_amp * recon_amp
@@ -150,19 +156,21 @@ for k in trange(num):
                 holophase = ((holo_phase + max_phs / 2) % max_phs) / max_phs
                 holo = numpy.uint8(holophase.cpu().data.numpy() * 255)
                 b = '4\\' + method + str(c)
-                imgpath = b + '.png'
+                imgpath = './results/'+b + '.png'
                 cv2.imwrite(imgpath, holo)
+    if k % 10 == 0:
+        torch.save(net.state_dict(), './checkpoint/'+method+'{}.pth'.format(k))
 
-        l.append(currentloss / validnum)
-        print('validloss:', currentloss / validnum)
+    l.append(currentloss / validnum)
+    print('validloss:', currentloss / validnum, 'avgpsnr:', currentpsnr / validnum)
     time.sleep(1)
 pass
 
-pthname = method + '.pth'
+pthname = method + '10.pth'
 lossname = method + 'avgloss.mat'
 tlossname = method + 'avgtloss.mat'
 
-torch.save(net.state_dict(), pthname)
+# torch.save(net.state_dict(), pthname)
 
 l = numpy.mat(l)
 io.savemat(lossname, {'avgloss': l})
